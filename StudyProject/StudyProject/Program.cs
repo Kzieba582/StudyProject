22fc53f67cf6250d3fbfac9f944f74f00375b3e4
Kzieba582 <62712336+Kzieba582@users.noreply.github.com>
2020-08-25 19:38:38 +0200

    Add files via upload

---
+++ StudyProject/StudyProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj\n\n 1. Zgadnij liczbe \n 2. Wyjdz\n");
+        Wybor:
+            int opcja = Convert.ToInt16(Console.ReadLine());
+
+            switch (opcja)
+            {
+                case 1:
+                    Console.WriteLine("Zgadnij liczbe!\n");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.WriteLine("Do zobaczenia.\n");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Brak opcji.\n");
+                    goto Wybor;
+            }
+        }
+    }
+}
