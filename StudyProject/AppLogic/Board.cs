b54d5550ce4b2d1b75b81bf0120ca89f13da7d2b
Kzieba582 <62712336+Kzieba582@users.noreply.github.com>
2020-08-30 17:23:05 +0200

    Add files via upload

---
+++ StudyProject/AppLogic/Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic
+{
+    public class Board
+    {
+        public string name { get; set; }
+        public string level { get; set; }
+        public string time { get; set; }
+        public string progress { get; set; }
+
+        public Board(string Name, string Level, string Time, string Progress)
+        {
+            Name = name;
+            Level = Level;
+            Time = Time;
+            Progress = Progress;
+        }
+    }
+
+    
+}
